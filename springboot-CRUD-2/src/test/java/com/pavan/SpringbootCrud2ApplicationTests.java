fbd8ae774f2d439b7aaa15363384f79fee1289ec
pavanmac_237539 <pavanmac_237539@localhost>
2022-08-14 20:45:41 +0530

    Committed Springboot_CURD

---
+++ springboot-CRUD-2/src/test/java/com/pavan/SpringbootCrud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
